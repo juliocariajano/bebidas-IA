2901e87b9c3a7dbedff56f3b85274222f00e30f7
Julio Cariajano <93696214+juliocariajano@users.noreply.github.com>
2025-09-17 21:11:55 +0200

    Creacion de Header con campos de busqueda

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,15 @@
-import { NavLink } from 'react-router-dom'
+import { useMemo } from 'react'
+import { NavLink, useLocation } from 'react-router-dom'
 
 export default function Header() {
+
+    const {pathname} = useLocation()
+
+    const isHome = useMemo(()=>pathname === '/', [pathname])
+    console.log(isHome)
+
     return (
-        <header className="bg-slate-800">
+        <header className= {isHome ? "bg-[url(/bg.jpg)] bg-cover bg-center" : "bg-slate-800" }>
             <div className="mx-auto container px-5 py-16">
                 <div className="flex justify-between items-center">
                     <div>
@@ -28,6 +35,48 @@ export default function Header() {
 
                 </div>
 
+                {
+                    isHome && (
+                        <form 
+                        className='md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6'
+                        
+                        >
+                            <div className='space-y-4'>
+                                <label
+                                    htmlFor='ingredient'
+                                    className='block text-white uppercase font-extrabold text-lg'
+                                >Nombre o Ingredientes</label>
+
+                            <input
+                                id='ingredient'
+                                type='text'
+                                name='ingredient'
+                                className='p-3 w-full rounded-lg focus:outline-none bg-white text-black'
+                                placeholder='Nombre o Ingredientes. Ej. Vodka, Tequila, Café'
+                            />
+                            </div>
+
+                             <div className='space-y-4'>
+                                <label
+                                    htmlFor='ingredient'
+                                    className='block text-white uppercase font-extrabold text-lg'
+                                >Categoria</label>
+
+                            <select
+                                id='ingredient'
+                                name='ingredient'
+                                className='p-3 w-full rounded-lg focus:outline-none bg-white text-black'
+                            >
+                                <option value=""> --Seleccione ------</option>
+                            </select>
+                            </div>
+                            <input 
+                                type='submit'
+                                value='Buscar Recetas'
+                                className='cursor-pointer bg-orange-800 hover:bg-orange-900 text-white font-extrabold w-full p-2 rounded-lg uppercase'
+                            />
+                        </form>
+                    )}
             </div>
 
         </header>
